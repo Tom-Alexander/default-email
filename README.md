cfd6728942abfdba92e5aaa75cfd6ba8acf11753
Tom-Alexander <me@tomalexander.co.nz>
2014-07-13 16:38:15 +1200

    INITIAL

---
+++ README.md
@@ -0,0 +1,25 @@
+# default-email
+
+Used to create HTML email templates for email marketing services. Based on this [email boilerplate](https://github.com/seanpowell/Email-Boilerplate).
+
+## Installation
+
+``` 
+npm install
+```
+
+## Usage
+
+Create your template in the ```./source``` directory. 
+
+### watch
+
+```
+gulp watch
+```
+
+### build
+
+```
+gulp build 
+```
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var gulp        = require('gulp'),
+    less        = require('gulp-less'),
+    inline      = require('gulp-inline-css'),
+    findReplace = require('gulp-frep'),
+    zip         = require('gulp-zip'),
+    watch       = require('gulp-watch'),
+    w3cjs       = require('gulp-w3cjs'),
+    patterns    = [{pattern: /(src="images\/)/g, replacement: 'src="'}];
+
+gulp.task('zip', function () {
+    return gulp.src('./production/out/*')
+            .pipe(zip('out.zip'))
+            .pipe(gulp.dest('./production'));
+});
+
+gulp.task('images', function () {
+    return gulp.src('.source/images/**/*')
+            .pipe(gulp.dest('./production/out'));
+});
+
+gulp.task('html', function () {
+    return gulp.src('./source/*.html')
+            .pipe(findReplace(patterns))
+            .pipe(inline())
+            .pipe(gulp.dest('./production/out'));
+});
+
+gulp.task('less', function () {
+    return gulp.src('source/less/style.less')
+            .pipe(less())
+            .pipe(gulp.dest('./production/css/'));
+});
+
+gulp.task('watch', function () {
+    watch({glob: ['source/less/*.less', 'source/*.html']}, function () {
+        gulp.src('source/*.html').pipe(w3cjs());
+        return gulp.src('source/less/style.less')
+            .pipe(less())
+            .pipe(gulp.dest('./production/css/'));
+    });
+});
+
+gulp.task('build', ['less', 'html', 'images', 'zip']);
